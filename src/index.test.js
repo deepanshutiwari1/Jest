2960c7575ffb4b789a47d655b79e4a5d9a86ff9b
deepanshu tiwari <deepanshu.tiwari1@thoughtworks.com>
2024-11-19 11:32:50 +0530

    feat: test object

--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,24 @@
 const multiply = require("./index");
 
-// Test Suite
-test("2 and 4 multiply to 8", () => {
-  expect(multiply(2, 4)).toBe(8); // exact equality
+// Test Suite // As many tests files as many Suits
+
+describe("Multiply test", () => {
+  it("2 and 4 multiply to 8", () => {
+    expect(multiply(2, 4)).toBe(8); // exact equality
+  });
+
+  it("3 and 5 multiply not to 14", () => {
+    // toBe is Exact equality like ===
+    expect(multiply(3, 5)).not.toBe(14);
+  });
+});
+
+describe("Objects", () => {
+  it("Object euqlity", () => {
+    const person = { name: "Mohan Kumar" };
+    person["age"] = 23;
+
+    // toEqual is deep equality
+    expect(person).toEqual({ name: "Mohan Kumar", age: 23 });
+  });
 });
